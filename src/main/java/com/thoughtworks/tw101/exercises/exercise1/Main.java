700a7644700fc87bfd60978f70ea3dbd3b2ba619
Storm Heidinger <stormheidinger@CliffordHeidinger-Mac.local>
2018-11-11 13:29:46 -0600

    Completed Exercise 1

--- src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
@@ -5,5 +5,12 @@ package com.thoughtworks.tw101.exercises.exercise1;
 public class Main {
     public static void main(String[] args) {
 
+        int j =0;
+
+        for (int i = 1; i < 100; i+= 2) {
+            System.out.print("Odd number: " + i +", ");
+            j = j+i;
+            System.out.print("Sum is: " + j + "\n");
+        }
     }
 }
