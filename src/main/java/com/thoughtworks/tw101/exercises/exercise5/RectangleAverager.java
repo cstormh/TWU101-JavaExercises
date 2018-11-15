dc25ab4849ee02473ef275323368afcfa6fd4a87
Storm Heidinger <cstormheidinger@gmail.com>
2018-11-15 11:34:59 -0600

    Completed Exercise 5

--- src/main/java/com/thoughtworks/tw101/exercises/exercise5/RectangleAverager.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise5/RectangleAverager.java
@@ -2,7 +2,16 @@ package com.thoughtworks.tw101.exercises.exercise5;
 
 public class RectangleAverager {
 
+    int totalArea;
+    float averageArea;
+
     public float averageArea(Rectangle[] rectangles) {
-        return 0;
+
+        for (Rectangle rectangle:rectangles) {
+            totalArea = totalArea + rectangle.area();
+        }
+
+        averageArea = totalArea / rectangles.length;
+        return averageArea;
     }
 }
