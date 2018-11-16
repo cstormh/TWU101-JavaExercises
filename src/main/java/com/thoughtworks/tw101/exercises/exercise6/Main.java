4f141a1b70e37bbc4b325499c1421867942e358f
Storm Heidinger <cstormheidinger@gmail.com>
2018-11-16 16:23:04 -0600

    Completed Exercise 6

--- src/main/java/com/thoughtworks/tw101/exercises/exercise6/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise6/Main.java
@@ -8,8 +8,21 @@ package com.thoughtworks.tw101.exercises.exercise6;
 // * Orcs are named “Orc” and have 20 initial hitpoints
 // * Trolls are named “Troll”, have 40 hit points and only take half damage
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        Orc orc1 = new Orc();
+        Troll troll1 = new Troll();
 
+        ArrayList<Monster> monsterArrayList = new ArrayList<>();
+
+        monsterArrayList.add(orc1);
+        monsterArrayList.add(troll1);
+
+        for (Monster monster:monsterArrayList) {
+            monster.takeDamage(10);
+            monster.reportStatus();
+        }
     }
 }
---
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise6/Orc.java
@@ -0,0 +1,27 @@
+package com.thoughtworks.tw101.exercises.exercise6;
+
+public class Orc implements Monster {
+    private int hitpoint = 20;
+    private String name = "Orc";
+
+    @Override
+    public void takeDamage(int amount) {
+
+        System.out.println("Attack for " + amount + " damage.");
+
+        int damage = amount;
+
+        System.out.println("Doing damage " + damage + " to monster.");
+
+        if (damage <= hitpoint)
+            hitpoint = hitpoint - damage;
+        else
+            hitpoint = 0;
+    }
+
+    @Override
+    public void reportStatus() {
+        System.out.println("Monster name is: " + name);
+        System.out.println("Monster hitpoint is: " + hitpoint);
+    }
+}
